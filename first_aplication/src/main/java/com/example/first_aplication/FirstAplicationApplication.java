332b4a4f038d46d8128d5cd9f2547c2edd853f54
Juan-Camilo-Suarez <juancamilsouarez3@gmail.com>
2022-04-13 17:40:21 +0300

    entitys

---
+++ first_aplication/src/main/java/com/example/first_aplication/FirstAplicationApplication.java
@@ -0,0 +1,13 @@
+package com.example.first_aplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstAplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstAplicationApplication.class, args);
+    }
+
+}
---
+++ first_aplication/src/test/java/com/example/first_aplication/FirstAplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.first_aplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstAplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
