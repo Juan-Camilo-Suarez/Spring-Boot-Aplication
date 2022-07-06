0aefa6d8196b953af5a0e586764de0f729e829d3
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-07-06 21:16:54 +0300

    get user

--- remember/src/main/java/ru/itis/remember/controller/Usercontroller.java
+++ remember/src/main/java/ru/itis/remember/controller/Usercontroller.java
@@ -1,12 +1,13 @@
 package ru.itis.remember.controller;
 
+import com.sun.istack.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.itis.remember.models.User;
 import ru.itis.remember.repositories.UserRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class Usercontroller {
@@ -16,5 +17,11 @@ public class Usercontroller {
     @GetMapping("/getusers")
     List<User> getAllUsers(){
         return userRepository.findAll();
+    }
+
+    @RequestMapping(value = "/user/{Id}", method = RequestMethod.GET)
+    public Optional<User> findById(@PathVariable @NotNull Long Id) {
+        return userRepository.findById(Id);
+
     }
 }
--- remember/src/main/java/ru/itis/remember/repositories/UserRepository.java
+++ remember/src/main/java/ru/itis/remember/repositories/UserRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 import ru.itis.remember.models.User;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends CrudRepository <User,Long> {
     List<User> findAll();
+    Optional<User> findById(Long id);
 }
